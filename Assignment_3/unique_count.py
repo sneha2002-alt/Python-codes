fad24bd3c0788a082e7ba04f51082ff0a3392d84
Python codes <snehamittal.sm22@gmail.com>
2026-04-07 18:28:02 +0530

    commit

--- Assignment_3/unique_count.py
+++ Assignment_3/unique_count.py
@@ -4,11 +4,8 @@ s = input("Enter a string: ")
 char_count = {}
 
 for char in s:
-  if char in char_count:
-    char_count[char] += 1
-  else:
-    char_count[char] = 1  
-    
+    char_count[char] = char_count.get(char, 0) + 1
+   
 print("Unique characters & their counts")
 
 for char, count in char_count.items():
